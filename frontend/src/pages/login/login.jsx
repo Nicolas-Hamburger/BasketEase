c489a4ab10fd6f53904d35202ca100f07d856b7d
Nicolas-Hamburger <nicolas132331@gmail.com>
2024-03-27 14:41:26 -0500

    create login page

--- frontend/src/pages/login/login.jsx
+++ frontend/src/pages/login/login.jsx
@@ -1,12 +1,22 @@
-import React from "react";
-import './style.css'
+import React from 'react';
+import { Grid, Typography } from '@material-ui/core';
+import './style.css';
 
-const LoginPage = () =>{
+function LoginPage() {
     return (
-        <div>
-            <h2>Login Page</h2>
+        <div className='container-login'>
+        <Grid container spacing={2}>
+            <Grid item xs={6}>
+                <div className='bg-login'>
+                <Typography>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla blanditiis iure quibusdam excepturi a modi earum ratione officiis nobis est. Eius dolores quisquam ipsum mollitia libero magni voluptas saepe corporis.</Typography>
+                </div>
+            </Grid>
+            <Grid item xs={6}>
+                <Typography>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum suscipit veritatis eum placeat optio accusamus magni enim quo, accusantium quod. Omnis totam, ullam est id enim quibusdam consequatur dolor officiis.</Typography>
+            </Grid>
+        </Grid>
         </div>
-    )
+    );
 }
 
 export default LoginPage;
