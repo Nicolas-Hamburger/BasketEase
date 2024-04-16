493b33cec120642121457d1a58c1e0f8d46acb8d
Nicolas-Hamburger <nicolas132331@gmail.com>
2024-04-15 20:50:41 -0500

    Agregando routas en el main de inventory

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from routes.users_routes import router as users_router
 from routes.sale_routes import router as sale_router
 from routes.products_routes import router as products_router
+from routes.inventory_routes import router as inventory_router
 
 app = FastAPI()
 
@@ -22,6 +23,7 @@ app.add_middleware(
 app.include_router(users_router)
 app.include_router(sale_router)
 app.include_router(products_router)
+app.include_router(inventory_router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)    
